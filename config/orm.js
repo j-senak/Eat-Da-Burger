8f1359b100a7ead254c6dcf7c306a4aa07c9f8f3
JohnS <senakj2011@hotmail.com>
2020-04-09 21:12:59 -0400

    Added update function to ORM

--- config/orm.js
+++ config/orm.js
@@ -49,5 +49,21 @@ const orm = {
             cb(result);
         });
     },
+    update: function(table, objColVals, condition, cb) {
+        const queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        console.log(queryString);
+
+        connection.query(queryString, function(err, res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    },
     
 }
